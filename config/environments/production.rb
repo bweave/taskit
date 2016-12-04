1cc36870f9fb0e182c5fcf6181bb230577a09bb1
Brian Weaver <brian.weaver@northpoint.org>
2016-12-04 14:14:52 -0500

    Hardcodes allowed_request_origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ENV['ALLOWED_REQUEST_ORIGINS']]
+  config.action_cable.allowed_request_origins = ['https://taskitapp.herokuapp.com', /http:\\/\\/taskitapp.herokuapp.com.*/]
   config.web_socket_server_url = ENV['WSS_URL']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
